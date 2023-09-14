1fc09043b1cbd56bc11db50f954c23c904f3cc86
NonokEE <shshrdl@naver.com>
2023-09-14 14:44:59 +0900

    UI: leverNeutral fix

--- Assets/01.Scripts/UI/JoystickLever.cs
+++ Assets/01.Scripts/UI/JoystickLever.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:906aaccb9b1cb9e58f13d8822006e75c973e8a1fb78fd43eb0813e7a6de5d2b5
-size 2294
+oid sha256:e5331348fdaa44f4ad71025fc070f3033297b0ff0e4c08ca0240cf49476e5c06
+size 2249
