73121eff2b5a97d8cfce4508f4a97f869863811b
NonokEE <shshrdl@naver.com>
2023-09-14 17:26:09 +0900

    PlayerMovement: fix iss1

--- Assets/01.Scripts/UI/JoystickLever.cs
+++ Assets/01.Scripts/UI/JoystickLever.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a4267e9e1df5bc484de431a26a53f46f71c1c6eb8d4797e79a71c1c8e6dff41
-size 2614
+oid sha256:c9b6acfb792d398175ad4cbc852ed6366ddfe1749a3abf06ad5e03f4f372b0ea
+size 2884
